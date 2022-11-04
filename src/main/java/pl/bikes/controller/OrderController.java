db488cd6e4a69fa2e885d6fde2925561e1cf641b
StaszekKosior <kosior.stanislaw@gmail.com>
2022-11-04 18:20:33 +0100

    #update ilości porduktów w bazie po zakończonym zakupie

--- src/main/java/pl/bikes/controller/OrderController.java
+++ src/main/java/pl/bikes/controller/OrderController.java
@@ -7,6 +7,7 @@ import pl.bikes.model.Order;
 import pl.bikes.model.Product;
 import pl.bikes.model.User;
 import pl.bikes.repository.OrderRepository;
+import pl.bikes.repository.ProductRepository;
 
 import javax.servlet.http.HttpSession;
 import java.util.Map;
@@ -16,7 +17,8 @@ import java.util.Map;
 @RequestMapping("/order")
 public class OrderController {
 
-    private final OrderRepository repository;
+    private final OrderRepository orderRepository;
+    private final ProductRepository productRepository;
 
     private final HttpSession session;
 
@@ -31,11 +33,21 @@ public class OrderController {
         Map<Product, Integer> cart = (Map<Product, Integer>) session.getAttribute("cartContents");
         User user = (User) session.getAttribute("loggedUser");
         Double value = (Double) session.getAttribute("cartValue");
-        repository.save(new Order(user, cart, value));
+        orderRepository.save(new Order(user, cart, value));
+        productUpdate();
+
         session.removeAttribute("cartContents");
         return "redirect:/";
     }
 
-
+    private void productUpdate() {
+        Map<Product, Integer> map = (Map<Product, Integer>) session.getAttribute("cartContents");
+        for (Map.Entry<Product, Integer> entry : map.entrySet()) {
+            Product product = entry.getKey();
+            Integer value = entry.getValue();
+            product.setQuantity(product.getQuantity() - value);
+            productRepository.save(product);
+        }
+    }
 
 }
